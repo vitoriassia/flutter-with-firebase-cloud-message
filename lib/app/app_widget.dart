5d3c7ed42b662944a7a1f0ddb028758347542f4c
Vitor Ferreira Iassia <iassiavitor@gmail.com>
2021-11-08 15:33:06 -0300

    removendo warning

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,5 +1,4 @@
 import 'package:app_modular/app/core/services/push_notification_service.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
